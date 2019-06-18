c7ad4a73b9a347556d22e2e6dd7da2c844e0eaa5
wuchongcheng <563576277@qq.com>
2019-06-18 22:39:16 +0800

    test-dev-2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import com.example.demo.reuse.ClassA;
+import com.example.demo.reuse.ClassB;
+import com.example.demo.reuse.ClassC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +13,15 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	private String testMethod() {
+		ClassA classA = new ClassA();
+		ClassC classC = new ClassC();
+		ClassB classB= new ClassB();
+		int classAHashCode = classA.hashCode();
+		int classBHashCode = classC.hashCode();
+		classBHashCode = new ClassA().hashCode();
+		int sum = classAHashCode + classBHashCode + 654654;
+		System.out.println(sum + 6476);
+		return "method end";
+	}
 }
